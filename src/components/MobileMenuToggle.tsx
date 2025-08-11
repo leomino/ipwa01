e1cc87bf7fb16de08afa86c8f45e7faf9e365990
Leonardo Palomino <l.palomino1@icloud.com>
2025-08-11 21:17:45 +0200

    feat: add functional nav links to mobilemenu

--- src/components/MobileMenuToggle.tsx
+++ src/components/MobileMenuToggle.tsx
@@ -7,19 +7,21 @@ export default function MobileMenu() {
     return (
         <>
             <button
-                onClick={() => setOpen(!open)}
-                aria-expanded={open}
-                aria-label="Toggle menu"
+                onClick={() => setOpen(true)}
             >
                 <img src={mobileMenu.src} alt="nav-menu" />
             </button>
 
             {open && (
-                <div className="sm:hidden flex flex-col px-5 mt-3 gap-3">
-                    <a href="/" className="hover:underline">Was ist der Klimawandel?</a>
-                    <a href="/emissions" className="hover:underline">CO₂-Emissionen</a>
-                    <a href="/about" className="hover:underline">Über uns</a>
+                <div className="sm:hidden absolute top-0 right-0 bg-black/50 h-screen w-screen z-1">
+                    <div className="sm:hidden flex flex-col gap-8 absolute top-0 right-0 bg-white h-1/3 w-screen z-2 items-center justify-center">
+                        <button className="absolute top-0 right-0 p-10" onClick={() => setOpen(false)}>X</button>
+                        <a href="/">Was ist der Klimawandel?</a>
+                        <a href="/emissions">CO₂-Emissionen</a>
+                        <a href="/about">Über uns</a>
+                    </div>
                 </div>
+
             )}
         </>
     );
